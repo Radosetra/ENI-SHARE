7a1ab37d2f3b92c2d7916330a577ddd1676fb866
to <rtodesire@gmail.com>
2023-03-05 00:46:57 +0300

    popopopopo

--- ProjectDevHunt/app/Http/Controllers/CommentController.php
+++ ProjectDevHunt/app/Http/Controllers/CommentController.php
@@ -22,68 +22,6 @@ class CommentController extends Controller
         ]);
     }
 
-    public function create(Request $request)
-    {
-        // Get data from form
-        $content = $request->input('content');
-        $user_id = $request->input('user_id');
-        $pub_id = $request->input('pub_id');
-
-        // Create new comment
-        $comment = new Comment;
-        $comment->content = $content;
-        $comment->comment_date = now(); // Current date and time
-        $comment->user_id = $user_id;
-        $comment->pub_id = $pub_id;
-        $comment->save();
-
-        // Redirect to corresponding article page
-        return redirect('/publications/' . $pub_id);
-    }
-
-    // public function edit(Request $request, $com_id)
-    // {
-    //     // Get comment to edit
-    //     $comment = Comment::find($com_id);
-
-    //     // Check if comment exists
-    //     if (!$comment) {
-    //         abort(404);
-    //     }
-
-    //     // Check if user has permission to edit comment
-    //     if ($comment->user_id != $request->user()->id) {
-    //         abort(403, 'You are not authorized to edit this comment.');
-    //     }
-
-    //     // Update comment content
-    //     $comment->content = $request->input('content');
-    //     $comment->save();
-
-    //     // Redirect to corresponding article page
-    //     return redirect('/publications/' . $comment->pub_id);
-    // }
-
-    // public function delete(Request $request, $com_id)
-    // {
-    //     // Get comment to delete
-    //     $comment = Comment::find($com_id);
-
-    //     // Check if comment exists
-    //     if (!$comment) {
-    //         abort(404);
-    //     }
-
-    //     // Check if user has permission to delete comment
-    //     if ($comment->user_id != $request->user()->id) {
-    //         abort(403, 'You are not authorized to delete this comment.');
-    //     }
-
-    //     // Delete comment
-    //     $comment->delete();
-
-    //     // Redirect to corresponding article page
-    //     return redirect('/publications/' . $comment->article_id);
-    // }
+    
 }
 
