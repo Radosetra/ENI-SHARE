c7f20b912c0a274185ae86e85bac1c03e204e4e3
Rado <mickaelradosetraraveloson@gmail.com>
2023-03-05 23:28:42 +0300

    InputPoste

---
+++ ProjectDevHunt/resources/js/components/InputPoste.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Publication</h5>
+            <!-- General Form Elements -->
+            <form>
+                <div class="row mb-3">
+                    <label for="inputText" class="col-sm-2 col-form-label">Titre</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" v-model="post.title">
+                    </div>
+                </div>
+
+                <tinyEditor v-model="post.description"></tinyEditor>
+
+                <div class="row mb-3">
+                    <div class="col-sm-2">
+                        <button type="submit" class="btn btn-primary">Poster</button>
+                    </div>
+                </div>
+
+            </form><!-- End General Form Elements -->
+
+        </div>
+    </div>
+</template>
+<script>
+import TinyEditor from './TinyEditor.vue';
+
+export default {
+    components: {
+        TinyEditor
+    },
+    data() {
+        return {
+            post: {
+                title: '',
+                description: ''
+            }
+        }
+    }
+}
+</script> 
---
+++ ProjectDevHunt/resources/js/components/pubButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <!-- pour acceder au champs poste -->
+    <div class="content-wrapper">
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <h2 class="text-center display-4">Post</h2>
+                <div class="row">
+                    <div class="col-md-8 offset-md-2">
+                        <form action="simple-results.html">
+                            <div class="input-group">
+                                <input type="search" class="form-control form-control-lg"
+                                    placeholder="I have an question">
+                                <div class="input-group-append">
+                                    <button type="submit" class="btn btn-lg btn-default">
+                                        <i class="fa fa-pencil"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
