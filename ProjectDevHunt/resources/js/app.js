83a9ffe06d286c0b5921c2991e1dcbb82291a951
Rado <mickaelradosetraraveloson@gmail.com>
2023-03-05 10:33:29 +0300

    Installation Vue

--- ProjectDevHunt/resources/js/app.js
+++ ProjectDevHunt/resources/js/app.js
@@ -2,6 +2,16 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
+
+// Vue
+import {createApp} from 'vue'
+
+import App from './App.vue'
+
+createApp(App).mount("#app")
+// Vue end
+
+
 window.Alpine = Alpine;
 
 Alpine.plugin(focus);
--- ProjectDevHunt/resources/views/layouts/guest.blade.php
+++ ProjectDevHunt/resources/views/layouts/guest.blade.php
@@ -16,7 +16,9 @@
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
+            
             {{ $slot }}
+            
         </div>
     </body>
 </html>
